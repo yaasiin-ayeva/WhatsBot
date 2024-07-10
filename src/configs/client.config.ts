232854424d8c4a6585d0df08123b9714dafbd2e4
Yaasiin <ayevayaasiin@gmail.com>
2024-07-10 01:00:20 +0000

    update : migrate project to wwebjs-webpack-exodus fix

--- src/configs/client.config.ts
+++ src/configs/client.config.ts
@@ -4,12 +4,8 @@ import EnvConfig from "./env.config";
 export const ClientConfig = {
     authStrategy: new LocalAuth(),
     puppeteer: {
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
         executablePath: EnvConfig.CHROME_PATH
     },
-    webVersionCache: {
-        remotePath: "https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2412.54v2.html",
-        // remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-        type: 'remote'
-    }
 }
